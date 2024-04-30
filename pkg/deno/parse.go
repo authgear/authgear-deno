9ded4e662684941b01033b1b51cc3c9e94838d05
Louis Chan <louischan@oursky.com>
2024-04-30 16:18:53 +0800

    Upgrade to 1.31.3

--- pkg/deno/parse.go
+++ pkg/deno/parse.go
@@ -4,9 +4,24 @@ import (
 	"regexp"
 )
 
-var accessToRegexp = regexp.MustCompile(`⚠️  ┌ Deno requests (.+) access to "(.+)"\.`)
-var allAccessRegexp = regexp.MustCompile(`⚠️  ┌ Deno requests (.+) access\.`)
-var hrtimeRegexp = regexp.MustCompile(`⚠️  ┌ Deno requests access to high precision time\.`)
+// On deno < 1.31.0
+// The permission prompt looks like
+//
+// ⚠️  ┌ Deno requests net access to "0.0.0.0:8080".
+//    ├ Requested by `Deno.listen()` API
+//    ├ Run again with --allow-net to bypass this prompt.
+//    └ Allow? [y/n] (y = yes, allow; n = no, deny) >
+//
+// On deno >= 1.31.0
+// The permission prompt looks like
+//
+// ┌ ⚠️  Deno requests net access to "0.0.0.0:8080".
+// ├ Requested by `Deno.listen()` API
+// ├ Run again with --allow-net to bypass this prompt.
+
+var accessToRegexp = regexp.MustCompile(`Deno requests (.+) access to "(.+)"\.`)
+var allAccessRegexp = regexp.MustCompile(`Deno requests (.+) access\.`)
+var hrtimeRegexp = regexp.MustCompile(`Deno requests access to high precision time\.`)
 
 func LineToPermissionDescriptor(line string) (*PermissionDescriptor, bool) {
 	if matches := hrtimeRegexp.FindStringSubmatch(line); len(matches) == 1 {
